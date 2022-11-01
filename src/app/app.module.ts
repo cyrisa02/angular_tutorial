96d479f5e75092f4691b971e5803a05c51fbf389
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2022-11-01 14:29:22 +0100

    Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { StatusComponent } from './status/status.component';
+import { LoginComponent } from './login/login.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { AccessRoutingModule } from './access/access-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    StatusComponent,
+    AddContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginComponent,
+    AccessRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
